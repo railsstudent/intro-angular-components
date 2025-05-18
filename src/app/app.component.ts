fbc97ecab0ded34dd13d134c6ec2c46a07320eaf
Connie Leung <cleung334@gmail.com>
2025-05-18 19:54:43 +0800

    feat: create CoffeePlanComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CoffeePlanComponent } from './coffee-plan/coffee-plan.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  imports: [CoffeePlanComponent],
+  template: `
+    <div class="content">
+      <h1 class="title">Coffee Plans</h1>
+
+      <h2 class="subtitle">We travel the world to source the very best single origin coffee for you</h2>
+
+      <div class="plans">
+        @for (plan of plans(); track plan) {
+          <app-coffee-plan [name]="plan" />
+        }
+      </div>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'intro-angular-components';
+  plans = signal(['The Single', 'The Curious', 'The Addict', 'The Hacker']);
 }
--- src/app/coffee-plan/coffee-plan.component.ts
+++ src/app/coffee-plan/coffee-plan.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-coffee-plan',
   imports: [],
-  templateUrl: './coffee-plan.component.html',
-  styleUrl: './coffee-plan.component.css'
+  template: `
+    <div class="plan">
+      <div class="description">
+        <span class="title"> {{ name() }} </span>
+      </div>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CoffeePlanComponent {
-
+  name = input('Default Plan');
 }
