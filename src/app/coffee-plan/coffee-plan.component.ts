c02d479a312a0e7a7fb09970ae0ecda5477a60ca
Connie Leung <cleung334@gmail.com>
2025-06-08 13:56:34 +0800

    refactor: dynamic content

--- src/app/coffee-plan/coffee-plan.component.ts
+++ src/app/coffee-plan/coffee-plan.component.ts
@@ -1,61 +1,25 @@
-import { ChangeDetectionStrategy, Component, input, output, computed } from '@angular/core';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import { matCoffeeOutline, matCoffeeMakerOutline, matEmojiFoodBeverageOutline, matFastfoodOutline } from '@ng-icons/material-icons/outline';
+import { NgTemplateOutlet } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input, output, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-coffee-plan',
-  imports: [NgIcon],
+  imports: [NgTemplateOutlet],
   template: `
     <div class="plan" (click)="selectPlan()" [class]="{ 'active-plan': selected() }">
-      @if (isShowCoffee()) {
-        <div class="coffee">
-          <ng-icon name="matCoffeeOutline" />
-          <ng-icon name="matCoffeeMakerOutline" />
-        </div>
-      }
+      <ng-container [ngTemplateOutlet]="coffee()" />
       <div class="description">
         <span class="title"> {{ name() }} </span>
       </div>
-      @if (isShowBeverage()) {
-        <div class="beverage">
-          <ng-icon name="matEmojiFoodBeverageOutline" />
-          <ng-icon name="matFastfoodOutline" />
-        </div>
-      }
+      <ng-container [ngTemplateOutlet]="beverage()" />
     </div>
   `,
-  styles: [`
-    .beverage {
-      display: flex;
-      flex-direction: column;
-      padding: 0.25rem;
-
-      > * {
-        width: 42px;
-        height: 42px;
-        color: green;
-      }
-    }
-    .coffee {
-      display: flex;
-      align-items: center;
-
-      > * {
-        width: 48px;
-        height: 48px;
-        color: brown;
-      }
-    }
-  `],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  viewProviders: [provideIcons({ matCoffeeOutline, matCoffeeMakerOutline, matEmojiFoodBeverageOutline, matFastfoodOutline })]
 })
 export class CoffeePlanComponent {
   name = input('Default Plan');
   selected = input(false);
-
-  isShowCoffee = computed(() => this.selected() && this.name().startsWith('The'));
-  isShowBeverage = computed(() => this.selected() && !this.name().startsWith('The'));
+  coffee = input<TemplateRef<any> | undefined>(undefined);
+  beverage = input<TemplateRef<any> | undefined>(undefined);
 
   selectedPlan = output<string>();
 
--- src/app/plan-picker/plan-picker.component.ts
+++ src/app/plan-picker/plan-picker.component.ts
@@ -1,23 +1,44 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { AddCoffeePlanComponent } from '../add-coffee-plan/add-coffee-plan.component';
 import { CoffeePlanComponent } from '../coffee-plan/coffee-plan.component';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { matCoffeeMakerOutline, matCoffeeOutline, matEmojiFoodBeverageOutline, matFastfoodOutline } from '@ng-icons/material-icons/outline';
 
 @Component({
   selector: 'app-plan-picker',
-  imports: [CoffeePlanComponent, AddCoffeePlanComponent],
+  imports: [CoffeePlanComponent, AddCoffeePlanComponent, NgIcon],
+  viewProviders: [provideIcons({ matCoffeeOutline, matCoffeeMakerOutline, matEmojiFoodBeverageOutline, matFastfoodOutline })],
   template: `
     <div class="plans">
       <app-add-coffee-plan (addCoffeePlan)="addPlan($event)" />
       {{selectedPlan()}}
       @for (plan of plans(); track plan) {
-        <app-coffee-plan [name]="plan" (selectedPlan)="handleSelectPlan($event)" [selected]="selectedPlan() === plan" />
+        @let coffeeIcons = showCoffeeIcons(plan) ? coffee : undefined; 
+        @let beverageIcons = showBeverageIcons(plan) ? beverage : undefined;
+        <app-coffee-plan [name]="plan" (selectedPlan)="handleSelectPlan($event)" [selected]="selectedPlan() === plan"
+          [beverage]="beverageIcons" [coffee]="coffeeIcons" />
       }
+
+      <ng-template #coffee>
+        <div class="coffee">
+          <ng-icon name="matCoffeeOutline" />
+          <ng-icon name="matCoffeeMakerOutline" />
+        </div>
+      </ng-template>
+
+      <ng-template #beverage>
+        <div class="beverage">
+          <ng-icon name="matEmojiFoodBeverageOutline" />
+          <ng-icon name="matFastfoodOutline" />
+        </div>
+      </ng-template>
     </div>
   `,
+  styleUrl: './plan-picker.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlanPickerComponent {
-  plans = signal(['The Single', 'The Curious', 'The Addict', 'The Hacker']);
+  plans = signal(['The Single', 'The Curious', 'The Addict', 'The Hacker', 'Vibe Coder']);
 
   selectedPlan = signal('');
 
@@ -27,5 +48,13 @@ export class PlanPickerComponent {
 
   addPlan(name: string) {
     this.plans.update((plans) => [...plans, name]);
+  }
+
+  showCoffeeIcons(name: string, pattern = 'The'): boolean {
+    return this.selectedPlan() === name && name.startsWith(pattern);
+  }
+
+  showBeverageIcons(name: string, pattern = 'The'): boolean {
+    return this.selectedPlan() === name && !name.startsWith(pattern);
   }
 }
